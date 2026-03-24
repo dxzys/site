4e37942a3895a4febe20608727be4255eba4e2a0
Cameron Steeves <76858431+dxzys@users.noreply.github.com>
2026-03-23 22:24:05 -0300

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,5 +5,25 @@
 
 <h1>About</h1>
 <p>
-  Under construction
+	Hey folks, I'm Cameron Steeves. I'm a student at the <a href="https://www.unb.ca/" target="_blank" rel="noopener noreferrer">University of New Brunswick</a>, where I'm majoring in Computer Science (technically, Science in Computer Science).
+</p>
+
+<p>
+  This is a site for me to write about things and share things I'm working on.
+</p>
+
+<p>Some things about me:</p>
+<ul>
+	<li>I was born in Saint John. My family is originally from rural <a href="https://en.wikipedia.org/wiki/Albert_County,_New_Brunswick" target="_blank" rel="noopener noreferrer">Albert County</a>.</li>
+	<li>I have interests in data science, music, technology, finance, among other topics.</li>
+	<li>In my free time, I like to study the history of my province and often find myself scouring old books, texts, and newspaper archives.</li>
+  <li>I also like to do photography. You can see photos that I've taken on my <a href="https://www.flickr.com/photos/198359054@N04/" target="_blank" rel="noopener noreferrer">Flickr profile</a>.</li>
+</ul>
+
+<p>
+  This summer I'll be working for <a href="https://www.alc.ca/" target="_blank" rel="noopener noreferrer">Atlantic Lottery</a> as a Data Engineer intern.
+</p>
+
+<p>
+  Sometimes I work on projects. I'm currently working on a webring for UNB students. See more <a href="https://github.com/dxzys/unb-webring" target="_blank" rel="noopener noreferrer">here</a>.
 </p>
