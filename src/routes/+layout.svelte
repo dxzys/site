5ff2589f4bad714283242cbbac834a24ab11d283
Cameron Steeves <76858431+dxzys@users.noreply.github.com>
2026-03-23 22:47:22 -0300

    Update +layout.svelte
    Add redirection logic on component mount using sessionStorage.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,18 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		const redirect = sessionStorage.redirect;
+		if (redirect) {
+			delete sessionStorage.redirect;
+			goto(redirect);
+		}
+	});
 </script>
 
 <svelte:head>
